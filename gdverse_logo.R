9e28f69f1467aa4987f0770ed0a02fdd395ac2fb
SpatLyu <lyu.geosocial@gmail.com>
2024-07-12 02:24:58 +0800

    update logo

--- gdverse_logo.R
+++ gdverse_logo.R
@@ -3,12 +3,31 @@ showtext_auto(enable = TRUE)
 font_add("ShineTypewriter", regular = "./ShineTypewriter-lgwzd.ttf")
 library(magick)
 
-image_read('./logo1.png') |> 
+library(hexSticker)
+sticker(
+  subplot = './logo1.png',
+  s_x = 0.97,
+  s_y = 0.98,
+  s_width = 1.5,
+  s_height = 1.5,
+  package = "gdverse",
+  p_family = "ShineTypewriter",
+  p_size = 25,
+  p_color = ggplot2::alpha("#ffffff",.85),
+  p_x = 1.05,
+  p_y = 1,
+  dpi = 300,
+  asp = 1,
+  h_size = 2.15,
+  h_color = ggplot2::alpha("#54564f",1),
+  h_fill = ggplot2::alpha('#ffffff',0),
+  white_around_sticker = T,
+  url = "https://spatlyu.github.io/gdverse",
+  u_color = "white",
+  u_size = 4.5,
+  filename = "gdverse_logo1.png"
+)
+
+image_read('./gdverse_logo1.png') |> 
   image_resize("256x256")|> 
-  image_annotate("gdverse",
-                 gravity = "northwest",
-                 location = "+84+111",
-                 color = ggplot2::alpha('#ffffff',0.8),
-                 size = 30,
-                 font = "ShineTypewriter") |> 
   image_write('./gdverse_logo.png')
